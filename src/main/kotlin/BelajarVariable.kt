a9e1e525382c640f03bc780ba8e435b004247125
Ajijagatsaputra <ajijagat90@gmail.com>
2021-11-02 12:20:09 +0700

    first commit

---
+++ src/main/kotlin/BelajarVariable.kt
@@ -0,0 +1,71 @@
+fun main(){
+//    var kardus = "Buku"
+//    kardus = "Pulpen"
+////    println(kardus)
+//
+//    // Bilangan Bulat
+//    var byte : Byte = 120
+//    var short : Short = 32_000
+//    var int : Int = 100_000_000
+//    var long : Long = 500_000_000
+//
+//
+//    // Bilangan Desimal
+//    var float : Float = 3.14F
+//    var double : Double = 3.1111111114
+//
+//
+//    // Tipe Data char
+//    var car : Char = 'A'
+//
+//    // Tipe data string
+//    var nama = "Aji"
+//    var alamat = """
+//        Desa Pagedangan
+//        Rt 08
+//        Rw 01
+//        Kecamatan Adiwerna
+//        Kabupaten Tegal
+//    """.trimIndent()
+//
+//    // Tipe data Boolean
+//    var benar : Boolean = true
+//    var salah : Boolean = false
+//
+//    // Belajar nullable
+//    var middlename : String? = null
+//    print(middlename)
+
+    // Biodata
+    var Nama = "Aji Jagat Saputra"
+    var kelas = "1B"
+    var NIM = "21090047"
+    var prodi = "Teknik Informatika"
+    var Biodata = """
+        nama = $Nama
+        kelas = $kelas
+        NIM = $prodi
+    """.trimIndent()
+     println(Biodata)
+     println("Jumlah karakter pada variabel biodata adalah ${Biodata.length}")
+
+
+
+//    println(byte)
+//    println(short)
+//    println(int)
+//    println(long)
+//    println(float)
+//    println(double)
+//    println(car)
+//    println(nama)
+//    println(alamat)
+//    println(benar)
+//    println(salah)
+
+
+//    val ember = "air"
+//    ember = "minyak"
+//    println(ember)
+
+}
